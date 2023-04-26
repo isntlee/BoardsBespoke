24935d62ab9517ce3cf3790a78c1e57b735f8ce6
DESKTOP-5FRD4TM\Lee <leedavidmartina@gmail.com>
2023-04-26 17:36:26 +0100

    Added accounts app

--- core/urls.py
+++ core/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from boards import views
+from accounts import views as accounts_views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('signup/', accounts_views.signup, name='signup'),
     path('boards/<pk>', views.board_topics, name='board_topics'),
     path('boards/<pk>/new', views.new_topic, name='new_topic'), 
 ]
