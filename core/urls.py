1b06b28ab258f97c340c9efeb878f964927ccf5c
DESKTOP-5FRD4TM\Lee <leedavidmartina@gmail.com>
2023-05-03 13:37:08 +0100

    Update view & tests added

--- core/urls.py
+++ core/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path('boards/<pk>/new', views.new_topic, name='new_topic'),
     path('boards/<pk>/topics/<topic_pk>', views.topic_posts, name='topic_posts'),
     path('boards/<pk>/topics/<topic_pk>/reply', views.reply_topic, name='reply_topic'),
+    path('boards/<pk>/topics/<topic_pk>/posts/<post_pk>/edit', views.PostUpdateView.as_view(), name='edit_post'),
 
 
     path('reset',
